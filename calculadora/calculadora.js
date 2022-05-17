346534bf6a82470d81d4f6d9f96ba7dc7c6fc99c
HenriqueTrigolo <79280717+HenriqueTrigolo@users.noreply.github.com>
2022-05-17 19:49:42 -0300

    Delete calculadora directory

--- calculadora/calculadora.js
+++
@@ -1,7 +0,0 @@
-function calculo(numero){
-    Calculator.visor.value = Calculator.visor.value + numero;
-}
-
-function remover(){
-    Calculator.visor.value = Calculator.visor.value.slice(0, -1);
-}
